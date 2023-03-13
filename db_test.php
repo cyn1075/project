0cbc1c855132736fa4e1e8c9bb99f527fdf847e3
cyn1075 <126940981+cyn1075@users.noreply.github.com>
2023-03-13 19:30:04 +0900

    Add files via upload

--- db_test.php
+++ db_test.php
@@ -2,6 +2,9 @@
 echo "mysql 연결 테스트<br>";
 
 $con = mysqli_connect('13.124.103.127', 'choi', 'choichoi', 'mysql','3306');
+$sql = "INSERT INTO project_user (id, name, email, phonenumber, password)
+VALUES('test', 'testname', 'test@naver.com', '010-9774-1838', '1111')";
+
 
 if($con){
     echo "connect : 성공<br>";
