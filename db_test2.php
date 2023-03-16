f2869f2728d64b08ab5babb4719b733c8de58586
cyn1075 <126940981+cyn1075@users.noreply.github.com>
2023-03-16 21:00:21 +0900

    Add files via upload

---
+++ db_test2.php
@@ -0,0 +1,23 @@
+<?php
+echo "mysql 연결 테스트<br>";
+
+$con = mysqli_connect('13.124.103.127', 'choi', 'choichoi', 'mysql','3306');
+
+$sql = "INSERT INTO project_user VALUES('', 'testname3', '3333')";
+
+
+echo $sql;
+$result = mysqli_query($con, $sql);
+
+if($con){
+    echo "connect : 성공<br>";
+}
+else{
+    echo "disconnect : 실패<br>";
+}
+ 
+$sql = 'SELECT VERSION()';
+$result = mysqli_query($con, $sql);
+$data = mysqli_fetch_assoc($result);
+echo $data['VERSION()'];
+?>
