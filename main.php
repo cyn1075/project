ee29602880903753472ad75b9debb560467fe7e7
cyn1075 <126940981+cyn1075@users.noreply.github.com>
2023-03-19 20:31:39 +0900

    Add files via upload

---
+++ main.php
@@ -0,0 +1,23 @@
+<?php
+?>
+
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Bob Escape Room</title>
+	<meta charset="utf-8">
+</head>
+
+
+<body>
+    <header>
+        <nav id = login_sign>
+            <li><a href ="main_login.php">로그인</a></li>
+            <li><a href ="main_signup.php">회원가입</a></li>
+        </nav>
+    </header>
+
+<div class="main">
+</div>
+</body>
+</html>
