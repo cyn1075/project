43f3c2fd75c790c08a8b5746adab3a0f5a2a18f1
cyn1075 <126940981+cyn1075@users.noreply.github.com>
2023-03-21 22:23:44 +0900

    Add files via upload

--- write_ok.php
+++ write_ok.php
@@ -4,10 +4,10 @@ include $_SERVER['DOCUMENT_ROOT']."/db.php";
 $username = $_POST['name'];
 $userpw = password_hash($_POST['pw'], PASSWORD_DEFAULT);
 $title = $_POST['title'];
-$connect = $_POST['content'];
+$content = $_POST['content'];
 $date = date('Y-m-d');
-if($username && $userpw && $title && $connect){
-    $sql = mq("INSERT INTO board(name,pw,title,content,date) values('".$username."','".$userpw."','".$title."','".$content."','".$date."')"); 
+if($username && $userpw && $title && $content){
+    $sql = mq("INSERT INTO board(name,pw,title,content,date) values('{$username}','{$userpw}','{$title}','{$content}','{$date}')"); 
     echo "<script>
     alert('글쓰기 완료되었습니다.');
     location.href='first_escape.php';</script>";
