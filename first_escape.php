ceeb65ffe7ab8776ca0350982478bf252124ad21
cyn1075 <126940981+cyn1075@users.noreply.github.com>
2023-03-22 21:18:26 +0900

    Add files via upload

--- first_escape.php
+++ first_escape.php
@@ -25,16 +25,44 @@
             </thead>
 
             <?php
-             $sql = mq("select * from board order by idx desc limit 0,5"); 
-             while($board = $sql->fetch_array())
+            if(isset($_GET['page'])){
+                $page = $_GET['page'];
+                  }else{
+                    $page = 1;
+                  }
+
+                  $sql = mq("select * from board");
+                  $row_num = mysqli_num_rows($sql);
+
+                  //한페이지에 보여질 개수
+                  $list = 3;
+                  //밑에 보여질 페이지 개수 
+                  $block_ct = 5;
+
+                  $block_num = ceil($page/$block_ct);
+                  $block_start = (($block_num - 1 ) * $block_ct) + 1;
+                  $block_end = $block_start + $block_ct -1;
+
+                  $total_page = ceil($row_num / $list);
+                  if($block_end > $total_pase) $block_end = $total_page;
+                  $total_block = ceil($total_page / $block_ct);
+                  $start_num = ($page-1) * $list;
+
+
+
+
+
+
+             $sql5 = mq("select * from board order by idx desc limit $start_num, $list"); 
+             while($board = $sql5->fetch_array())
              {
-               
                $title=$board["title"]; 
                if(strlen($title)>30)
                { 
                 
                  $title=str_replace($board["title"],mb_substr($board["title"],0,30,"utf-8")."...",$board["title"]);
                }
+               
             
             ?>
           <tbody>
@@ -48,6 +76,46 @@
             </tbody>
 <?php } ?>
 </table>
+
+
+<div id = "page_num">
+    <ul>
+        <?php 
+        if($page <=1)
+        {
+            echo "<li class='fo_re'>처음</li>";
+        }else{
+            echo "<li><a href = '?page=1'>처음</a></li>";
+        }
+        if($page <= 1)
+        {
+
+        }else{
+            $pre = $page-1;
+            echo"<li><a href = '?page=$pre'>이전</a></li>";
+        }
+        for($i=$block_start; $i<=$block_end; $i++){ 
+           
+            if($page == $i){ 
+              echo "<li class='fo_re'>[$i]</li>";
+            }else{
+              echo "<li><a href='?page=$i'>[$i]</a></li>";
+            }
+          }
+          if($block_num >= $total_block){ 
+          }else{
+            $next = $page + 1;
+            echo "<li><a href='?page=$next'>다음</a></li>";
+          }
+          if($page >= $total_page){ 
+            echo "<li class='fo_re'>마지막</li>";
+          }else{
+            echo "<li><a href='?page=$total_page'>마지막</a></li>";
+          }
+        ?>
+    </ul>
+</div>
+
         <div id="write_btn">
             <a href="writer.php"><button>후기 작성하기</button></a>
         </div>
