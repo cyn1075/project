c8ecb5809f5081622bcff0f7c87685bab5f8884a
cyn1075 <126940981+cyn1075@users.noreply.github.com>
2023-03-22 23:33:31 +0900

    Add files via upload

--- index.js
+++ index.js
@@ -8,22 +8,26 @@ window.initMap = function () {
         { label: "학원", name: "학원", lat: 37.271735, lng: 127.013979 }
     ];
 
-    const infowindow = new google.maps.InfoWindow();
-
-
+    const bounds = new google.maps.LatLngBounds();
+    const infoWindow = new google.maps.InfoWindow();
+  
     malls.forEach(({ label, name, lat, lng }) => {
-        const marker = new google.maps.Marker({
-          position: { lat, lng },
-          label,
-          map,
-        });
-        marker.addListener("click", () => {
-            map.panTo(marker.position);
-            infowindow.setContent(name);
-            infowindow.open({
-              anchor: marker,
-              map,
-            });
-          });
+      const marker = new google.maps.Marker({
+        position: { lat, lng },
+        label,
+        map
       });
+      bounds.extend(marker.position);
+  
+      marker.addListener("click", () => {
+        map.panTo(marker.position);
+        infoWindow.setContent(name);
+        infoWindow.open({
+          anchor: marker,
+          map
+        });
+      });
+    });
+  
+    map.fitBounds(bounds);
   };
