58bec3584e21f9638bc41b64ebadacc186da9bae
cyn1075 <126940981+cyn1075@users.noreply.github.com>
2023-03-31 14:53:31 +0900

    Add files via upload

--- checkid.php
+++ checkid.php
@@ -1,6 +1,6 @@
 <?php
 
-$db = new mysqli('13.124.103.127', 'choi', 'choichoi', 'mysql'); 
+$mysqli = new mysqli('13.124.103.127', 'choi', 'choichoi', 'mysql'); 
 
 
 $userid = $_POST['name'];
@@ -10,21 +10,26 @@ if(!$userid){
     alert('아이디를 입력해 주세요.');
     history.back();</script>";
 }else{
-    $sql = $db -> prepare("SELECT * FROM main_user WHERE id = :userid");
-    $sql -> bindParam(':userid', $userid);
-    $sql -> execute();
-    $count = $sql -> rowCount();
+    $sql = "SELECT * FROM main_user WHERE id='$username'";
+$result = $mysqli->query($sql);
 
-    if($count<1){
-        echo "<script>
-    alert('사용 가능한 아이디 입니다.');
-    history.back();</script>";
-    }else{
-        echo "<script>
+// 결과가 있으면 아이디 중복, 없으면 아이디 사용 가능
+if ($result->num_rows > 0) {
+    echo "<script>
     alert('이미 사용중인 아이디 입니다.');
     history.back();</script>";
-    }
+} else {
+    echo "<script>
+    alert('사용가능한 아이디 입니다.');
+    history.back();</script>";;
+}
+
+// MySQL 데이터베이스 연결 해제
+$mysqli->close();
 
 }
+
+
+
 
 ?>
