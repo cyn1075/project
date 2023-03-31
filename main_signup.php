affbd36e544d3ee0623519d42d345581e60c931a
cyn1075 <126940981+cyn1075@users.noreply.github.com>
2023-03-31 15:51:44 +0900

    Add files via upload

--- main_signup.php
+++ main_signup.php
@@ -19,6 +19,7 @@
         <div class="mb-3 ">
                 <label for="name" class="form-label">아이디</label>
                 <input type="name" name="name" class="form-control" id="name" placeholder="아이디를 입력해 주세요.">
+
             </div>
             <div class="mb-3 ">
                 <label for="password" class="form-label">비밀번호</label>
@@ -39,36 +40,7 @@
         const password = document.querySelector("#password");
         const passwordCheck = document.querySelector("#password-check");
         signupButton.addEventListener("click", function(e) {
-            <?php
-            $mysqli = new mysqli('13.124.103.127', 'choi', 'choichoi', 'mysql'); 
-            $userid = $_POST['name'];
-
-            if(!$userid){
-                echo "<script>
-                alert('아이디를 입력해 주세요.');
-                history.back();</script>";
-           }else{
-               $sql = "SELECT * FROM main_user WHERE id='$username'";
-               $result = $mysqli->query($sql);
-
-               // 결과가 있으면 아이디 중복, 없으면 아이디 사용 가능
-            if ($result->num_rows > 0) {
-                echo "<script>
-                alert('이미 사용중인 아이디 입니다.');
-                history.back();</script>";
-            } else {
-            echo "<script>
-            alert('사용가능한 아이디 입니다.');
-            history.back();</script>";;
-            }
-        
-             // MySQL 데이터베이스 연결 해제
-             $mysqli->close();
-
-            }
-            
-            ?>
-            else if(password.value&& password.value === passwordCheck.value){
+            if(password.value&& password.value === passwordCheck.value){
                 
             signupForm.submit();
             location.href ="db_main.php"
@@ -76,8 +48,6 @@
                 alert("비밀번호가 서로 일치하지 않습니다");
             }
         });
-
-        
     </script>
 
 </body>
