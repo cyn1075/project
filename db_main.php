27d1b2817f0eac218547b453035783936517888a
cyn1075 <126940981+cyn1075@users.noreply.github.com>
2023-03-31 17:27:30 +0900

    Add files via upload

--- db_main.php
+++ db_main.php
@@ -7,8 +7,7 @@ $pw = $_POST['password'];
 $pwchk = $_POST['password-check'];
 
 
-$sql = "INSERT INTO main_user (id, pw) VALUES('$id', '{$_POST['password']}')";
-$result = mysqli_query($con, $sql);
+
 
 if($pw != $pwchk){
     echo "<script>
@@ -20,22 +19,18 @@ if($pw != $pwchk){
     history.back();</script>";
 }else if(isset($id)){
     $sql2 = "SELECT * FROM main_user WHERE id='$id'";
-    $result2 = $mysqli->query($sql2);
+    $result = $mysqli->query($sql2);
     if ($result->num_rows > 0) {
         echo "<script>
         alert('이미 사용중인 아이디 입니다.');
         history.back();</script>";
     } else {
         echo "<script>
-    alert('회원가입 됐습니다.');
-    
+    alert('회원가입 됐습니다.'); 
     location.href='index.php';</script>";
     }
-}
-    
- 
-$sql = 'SELECT VERSION()';
-$result = mysqli_query($con, $sql);
-$data = mysqli_fetch_assoc($result);
 
+    $sql = "INSERT INTO main_user (id, pw) VALUES('$id', '{$_POST['password']}')";
+    $result = mysqli_query($con, $sql);
+}
 ?>
