bb5a33243cad2a24bb1000058ea0d3f302f951ba
cyn1075 <126940981+cyn1075@users.noreply.github.com>
2023-04-01 20:58:40 +0900

    Add files via upload

--- read.php
+++ read.php
@@ -20,19 +20,44 @@ include $_SERVER['DOCUMENT_ROOT']."/db.php";
 <div id="board_read">
 	<h2><?php echo $board['title']; ?></h2>
 		<div id="user_info">
-			<?php echo $board['name']; ?> <?php echo $board['date']; ?> 조회:<?php echo $board['hit']; ?>
+			<?php echo $board['name']; ?> 
+			<?php echo $board['date']; ?> 조회:<?php echo $board['hit']; ?>
 				<div id="bo_line"></div>
 			</div>
 			<div id="bo_content">
 				<?php echo nl2br("$board[content]"); ?>
 			</div>
 	<div id="bo_ser">
+<?php 
+session_start();
+if($_SESSION['userid'] == $board['name']){
+?>
+        <ul>
+			<li><a href="first_escape.php">[목록으로]</a></li>
+			<li><a href="modify.php?idx=<?php echo $board['idx']; ?>">[수정]</a></li>
+			<li><a href="delete.php?idx=<?php echo $board['idx']; ?>">[삭제]</a></li>
+		</ul>
+
+<?php }else{
+	?>
+	<ul>
+	<li><a href="first_escape.php">[목록으로]</a></li>
+	</ul>
+<?php }
+?>
+	
+
+
+
 		<ul>
 			<li><a href="first_escape.php">[목록으로]</a></li>
 			<li><a href="modify.php?idx=<?php echo $board['idx']; ?>">[수정]</a></li>
 			<li><a href="delete.php?idx=<?php echo $board['idx']; ?>">[삭제]</a></li>
 		</ul>
-	</div>
+	
+		</div>
+
+		
 </div>
 </body>
 </html>
