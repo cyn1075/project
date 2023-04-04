f701f7452f46d6f23508cc8bf309369bf28934ad
cyn1075 <126940981+cyn1075@users.noreply.github.com>
2023-04-04 23:09:07 +0900

    Add files via upload

--- read_reservation.php
+++ read_reservation.php
@@ -1,19 +1,19 @@
 <!DOCTYPE html>
 <html>
 <head>
-	<title>Reservation Information</title>
+	<title>예약 정보</title>
 	<link rel="stylesheet" type="text/css" href="reservation.css">
 </head>
 <body>
-	<h1>Reservation Information</h1>
+	<h1>예약 정보</h1>
 	<table>
 		<tr>
-			<th>User ID</th>
-			<th>Phone</th>
-			<th>Menu</th>
-			<th>Date</th>
-			<th>Time</th>
-			<th>Number of People</th>
+			<th>아이디</th>
+			<th>전화번호</th>
+			<th>방탈출 이름</th>
+			<th>날짜</th>
+			<th>시간</th>
+			<th>인원수</th>
 		</tr>
 <?php
 $db = mysqli_connect('13.124.103.127', 'choi', 'choichoi', 'mysql');
@@ -23,9 +23,6 @@ $username = $_SESSION['userid'];
 
 $sql = "SELECT * FROM reservation WHERE user_id='$username'";
 $result = mysqli_query($db, $sql);
-
-// echo "<table>";
-// echo "<tr><th>Phone</th><th>Menu</th><th>Date</th><th>Time</th><th>Number of People</th></tr>";
 
 while($row = mysqli_fetch_array($result)) {
     echo "<tr><td>".$row["user_id"]."</td><td>".$row["phone"]."</td><td>".$row["menu"]."</td><td>".$row["date"]."</td><td>".$row["time"]."</td><td>".$row["num_people"]."</td></tr>";
