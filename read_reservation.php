bd12d2ecc363abe805982d5f0fdc42b0c2b8561b
cyn1075 <126940981+cyn1075@users.noreply.github.com>
2023-04-04 22:56:46 +0900

    Add files via upload

--- read_reservation.php
+++ read_reservation.php
@@ -1,3 +1,20 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Reservation Information</title>
+	<link rel="stylesheet" type="text/css" href="reservation.css">
+</head>
+<body>
+	<h1>Reservation Information</h1>
+	<table>
+		<tr>
+			<th>User ID</th>
+			<th>Phone</th>
+			<th>Menu</th>
+			<th>Date</th>
+			<th>Time</th>
+			<th>Number of People</th>
+		</tr>
 <?php
 $db = mysqli_connect('13.124.103.127', 'choi', 'choichoi', 'mysql');
 
@@ -11,9 +28,11 @@ echo "<table>";
 echo "<tr><th>Phone</th><th>Menu</th><th>Date</th><th>Time</th><th>Number of People</th></tr>";
 
 while($row = mysqli_fetch_array($result)) {
-    echo "<tr><td>".$row['phone']."</td><td>".$row['menu']."</td><td>".$row['date']."</td><td>".$row['time']."</td><td>".$row['num_people']."</td></tr>";
+    echo "<tr><td>".$row["user_id"]."</td><td>".$row["phone"]."</td><td>".$row["menu"]."</td><td>".$row["date"]."</td><td>".$row["time"]."</td><td>".$row["num_people"]."</td></tr>";
 }
-
 echo "</table>";
 mysqli_close($db);
 ?>
+</table>
+</body>
+</html>
