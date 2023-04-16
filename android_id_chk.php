1df867c734ba454ccbb7d30f3d06cd885068a0da
cyn1075 <126940981+cyn1075@users.noreply.github.com>
2023-04-16 18:01:24 +0900

    Add files via upload

--- android_id_chk.php
+++ android_id_chk.php
@@ -8,10 +8,15 @@ $sql = "SELECT * FROM android_signup WHERE id='$id'" ;
 
 $result = $con->query($sql);
 
+$member = mysqli_fetch_array($result);
+
+print_r($member);
+
 if ($result->num_rows > 0) {
     echo 1;
 } else {
     echo 0;
+    echo $member['name'];
 }
 
 $mysqli->close();
