74acdfc48b613d1aaffb8d888be55f346bcbf730
cyn1075 <126940981+cyn1075@users.noreply.github.com>
2023-05-16 22:15:26 +0900

    Add files via upload

--- android_login.php
+++ android_login.php
@@ -7,13 +7,15 @@ $pw = $_POST['pw'];
 
 $sql = "SELECT * FROM android_signup WHERE id='$id' AND (pw ='$pw')";
 $sql2 = "SELECT name FROM android_signup WhERE id ='$id'";
+$sql3 = "SELECT image FROM android_signup WhERE id ='$id'";
 
-
+$result3 = mysqli_query($con, $sql3);
 $result2 = mysqli_query($con, $sql2);
 $result = mysqli_query($con, $sql);
 
 $member = mysqli_fetch_array($result);
 $member2 = mysqli_fetch_array($result2);
+$member3 = mysqli_fetch_array($result3);
 
 // print_r($member);
 
@@ -21,7 +23,7 @@ $member2 = mysqli_fetch_array($result2);
 if($member==0){
     echo 1;
 }else{
-    echo $member2['name'];
+    echo $member2['name'], $member3['image'];
     exit();
 }
 
