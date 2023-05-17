b3d0eb6c7ae702f3fc8832a3a96f4ed6a6c816c1
cyn1075 <126940981+cyn1075@users.noreply.github.com>
2023-05-18 02:25:58 +0900

    Add files via upload

--- android_call.php
+++ android_call.php
@@ -4,14 +4,16 @@ $con = mysqli_connect('13.124.103.127', 'choi', 'choichoi', 'mysql');
 
 $id = $_POST['userid'];
 
+$sql = "SELECT * FROM android_signup WHERE id='$id' AND (pw ='$pw')";
 $sql2 = "SELECT name FROM android_signup WhERE id ='$id'";
 $sql3 = "SELECT image FROM android_signup WhERE id ='$id'";
 
-$result3 = mysqli_query($con, $sql3);
+
+$result = mysqli_query($con, $sql);
 $result2 = mysqli_query($con, $sql2);
+$result3 = mysqli_query($con, $sql3);
 
-
-
+$member = mysqli_fetch_array($result);
 $member2 = mysqli_fetch_array($result2);
 $member3 = mysqli_fetch_array($result3);
 
