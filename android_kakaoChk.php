72280e024c3fa3d6bf5dba425bc4f5fed817abb6
cyn1075 <126940981+cyn1075@users.noreply.github.com>
2023-05-20 17:41:01 +0900

    Add files via upload

---
+++ android_kakaoChk.php
@@ -0,0 +1,24 @@
+<?php
+
+$con = mysqli_connect('13.124.103.127', 'choi', 'choichoi', 'mysql');
+
+$id = $_POST['userid'];
+
+$sql = "SELECT * FROM android_signup WHERE id='$id'";
+$sql2 = "SELECT id FROM android_signup WhERE kakaoid ='$id'";
+
+$result = mysqli_query($con, $sql);
+$result2 = mysqli_query($con, $sql2);
+
+$member = mysqli_fetch_array($result);
+$member2 = mysqli_fetch_array($result2);
+
+
+if($member==0){
+    echo 1;
+}else{
+    echo $member2['id'];
+    exit();
+}
+
+?>
