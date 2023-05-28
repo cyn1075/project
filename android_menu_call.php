006215b007eed754453e539a3a66098a49b7e012
cyn1075 <126940981+cyn1075@users.noreply.github.com>
2023-05-28 16:19:51 +0900

    Add files via upload

--- android_menu_call.php
+++ android_menu_call.php
@@ -5,37 +5,31 @@ $con = mysqli_connect('13.124.103.127', 'choi', 'choichoi', 'mysql');
 $name = $_POST['menuname'];
 
 $sql = "SELECT * FROM android_menu WHERE '$name'";
-$sql2 = "SELECT title FROM android_menu WhERE '$name'";
-$sql3 = "SELECT image FROM android_menu WhERE '$name'";
 
 
 $result = mysqli_query($con, $sql);
-$result2 = mysqli_query($con, $sql2);
-$result3 = mysqli_query($con, $sql3);
 
 $member = mysqli_fetch_array($result);
-$member2 = mysqli_fetch_array($result2);
-$member3 = mysqli_fetch_array($result3);
 
-// $response = array();
+$response = array();
 
+if (mysqli_num_rows($result) > 0) {
+    $row = mysqli_fetch_assoc($result);
+    $response['success'] = true;
+    $response['id'] = $row['id'];
+    $response['name'] = $row['name'];
+    $response['description'] = $row['description'];
+    
 
-if($member==0){
-    // $response['status'] = 1;
+    echo json_encode($response);
+} else {
+    $response['success'] = false;
+    $response['message'] = "No data found for the given member.";
 
-    echo 1;
-
-}else{
-    // $response['status'] = 0;
-    // $response['title'] = $member2['title'];
-    // $response['image'] = $member3['image'];
-    print_r($member);
+    echo json_encode($response);
 }
 
-header('Content-Type: application/json; charset=utf-8');
-echo json_encode($response, JSON_UNESCAPED_UNICODE);
-
-
-exit();
+mysqli_close($con);
+?>
 
 ?>
