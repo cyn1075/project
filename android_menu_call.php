567f5387ceeb7717bcd9e63bf73f32828790dd5d
cyn1075 <126940981+cyn1075@users.noreply.github.com>
2023-06-06 21:37:04 +0900

    Add files via upload

--- android_menu_call.php
+++ android_menu_call.php
@@ -4,7 +4,7 @@ $con = mysqli_connect('13.124.103.127', 'choi', 'choichoi', 'mysql');
 
 $name = $_POST['menuname'];
 
-$sql = "SELECT * FROM android_menu WHERE '$name'";
+$sql = " SELECT * FROM android_menu WHERE name = '$name' ";
 
 
 $result = mysqli_query($con, $sql);
@@ -14,9 +14,7 @@ $member = mysqli_fetch_array($result);
 $response = array();
 
 if ($member == 0) {
-
     echo 1;
-    
 } else {
    
     for($i=0;$i<$rowCnt;$i++){
