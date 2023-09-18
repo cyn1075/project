8e50e1dec0482b3eb9beeb65908700e5323e5ad8
cyn1075 <126940981+cyn1075@users.noreply.github.com>
2023-09-18 19:30:28 +0900

    Add files via upload

--- android_db.php
+++ android_db.php
@@ -1,51 +1,44 @@
 <?php
 
-// 데이터베이스 연결
+// MySQL 연결 설정
 $con = mysqli_connect('13.124.103.127', 'choi', 'choichoi', 'mysql');
 
-// 사용자로부터 입력 받은 데이터
+// POST 데이터 가져오기
 $id = $_POST['userId'];
 $pw = $_POST['userPw'];
 $phone = $_POST['userPhone'];
 $name = $_POST['userName'];
 $date = date('Y-m-d');
 
-// 파일 업로드 관련 정보
-$upload_dir = '/home/project'; // 사진을 저장할 디렉토리 경로를 지정해야 합니다.
+// 이미지 업로드 처리
+if(isset($_FILES['userImage'])){
+    $target_dir = "uploads/"; // 이미지를 저장할 디렉토리 경로
+    $target_file = $target_dir . basename($_FILES["userImage"]["name"]); // 업로드한 이미지 파일 경로
 
-// 'userImage' 필드에서 이미지 업로드 처리
-if(isset($_FILES['userImage']) && $_FILES['userImage']['error'] == 0){
-    $org_file = $_FILES['userImage']['name'];
-    $temp_mem = explode(".", $org_file);
-    $file_expt = $temp_mem[1];
-    
-    // 파일명 중복 검사 및 처리
-    $fix_filename = $temp_mem[0];
-    $fix_i = 0;
-    while(file_exists($upload_dir . $fix_filename . "." . $file_expt)){
-        $fix_i++;
-        $fix_filename = $temp_mem[0] . "(" . $fix_i . ")";
+    // 이미지를 지정한 디렉토리로 이동
+    if (move_uploaded_file($_FILES["userImage"]["tmp_name"], $target_file)) {
+        $image = $target_file; // 이미지 파일 경로를 데이터베이스에 저장
+    } else {
+        echo "이미지 업로드 실패";
+        exit();
     }
-    
-    // 이미지 업로드
-    $image = $fix_filename . "." . $file_expt;
-    move_uploaded_file($_FILES['userImage']['tmp_name'], $upload_dir . $image);
 } else {
-    // 'userImage' 필드에서 이미지가 업로드되지 않았을 경우 기본값 또는 오류 처리를 추가할 수 있습니다.
-    $image = ''; // 또는 기본 이미지 파일명을 설정할 수 있습니다.
+    echo "이미지가 전송되지 않았습니다.";
+    exit();
 }
 
 // SQL 쿼리 실행
-$sql = "INSERT INTO android_signup(id, pw, phone, image, name, date) VALUES ('$id', '$pw', '$phone', '$image', '$name', '$date')";
+$sql = "INSERT INTO android_signup(id, pw, phone, image, name, date) VALUES('$id', '$pw', '$phone', '$image', '$name', '$date')";
 $result = mysqli_query($con, $sql);
 
-// 결과 출력
+// 결과 반환
 if($result){
     echo "성공, $id";
 }else{
     echo "실패";
 }
 
-// 데이터베이스 연결 종료
+// MySQL 연결 종료
 mysqli_close($con);
+
 ?>
