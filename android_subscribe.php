e2b5cfafa972a8432528f7786ab4bd4fd4a037da
cyn1075 <126940981+cyn1075@users.noreply.github.com>
2023-10-05 01:24:59 +0900

    Add files via upload

---
+++ android_subscribe.php
@@ -0,0 +1,24 @@
+<?php
+
+$con = mysqli_connect('13.124.103.127', 'choi', 'choichoi', 'mysql');
+
+$myname = $_POST['myName'];
+$token = $_POST['token'];
+$subscribename = $_POST['subscribeName'];
+
+
+
+$sql = "INSERT INTO android_subscribe(myid, token, subscribeid) values('$myname', '$token', '$subscribename')";
+$result = mysqli_query($con, $sql);
+
+
+if($result){
+    echo "성공";
+}else{
+    echo"실패";
+}
+
+mysqli_close($con);
+
+
+?>
