a94ab5e19a009ec604dd58f29cef67e49c895109
cyn1075 <126940981+cyn1075@users.noreply.github.com>
2023-10-05 13:50:38 +0900

    Add files via upload

--- sendCommentFCMRequest.php
+++ sendCommentFCMRequest.php
@@ -1,8 +1,8 @@
 <?php
 define('API_ACCESS_KEY','AAAAc_IEV4A:APA91bFrBDHi3pbT7Qh8YmD3vfzlK4YQ-QZaO6Ce8LVfLrfiLK-gfRz9naUurejx8MqhItaR2HFDkRE_Av-Rad4yquHCrxufh7_BVTK3y4xw6b4qiGmRNIMoYz2l-9-tlnQ1QBGehGws');
  $fcmUrl = 'https://fcm.googleapis.com/fcm/send';
- $token = 'elvVfIsQTFCsf8jeHgCr9i:APA91bElBGRcjE0-9vbO8quiHwefEAFyZ-BvHi1Xwyx-fEDFmzEiwAcJz154yM_GSiFxQ_5gTw0aoS5bRhat2izorBv0CQevz-gBByHko2MRxYl4nJSsWm8lWIFViLGJBVOxlp6Mnya3';
-//  $token= $_POST["Token"]; //해당 Device TokenKey
+//  $token = 'elvVfIsQTFCsf8jeHgCr9i:APA91bElBGRcjE0-9vbO8quiHwefEAFyZ-BvHi1Xwyx-fEDFmzEiwAcJz154yM_GSiFxQ_5gTw0aoS5bRhat2izorBv0CQevz-gBByHko2MRxYl4nJSsWm8lWIFViLGJBVOxlp6Mnya3';
+ $token= $_POST["Token"]; //해당 Device TokenKey
     $notification = [
             'title' =>'title',
             'body' => 'body of message.',
