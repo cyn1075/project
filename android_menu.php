a393540641eb0af23c92ebf7dc9bc41699926122
cyn1075 <126940981+cyn1075@users.noreply.github.com>
2023-10-12 02:54:08 +0900

    Add files via upload

--- android_menu.php
+++ android_menu.php
@@ -17,11 +17,11 @@ if ($subscribeResult) {
     $header = array("Content-Type:application/json", "Authorization: key=AAAAc_IEV4A:APA91bFrBDHi3pbT7Qh8YmD3vfzlK4YQ-QZaO6Ce8LVfLrfiLK-gfRz9naUurejx8MqhItaR2HFDkRE_Av-Rad4yquHCrxufh7_BVTK3y4xw6b4qiGmRNIMoYzYz2l-9-tlnQ1QBGehGws");
 
 
-    $token = $subscribeResult->fetch_array(MYSQLI_ASSOC);
+    // $token = $subscribeResult->fetch_array(MYSQLI_ASSOC);
 
-    // while ($row = mysqli_fetch_assoc($subscribeResult)) {
-    // $token = $row['token'];
-    // echo $token;
+    while ($row = mysqli_fetch_assoc($subscribeResult)) {
+    $token = $row['token'];
+    echo $token;
 
     $data = json_encode(array(
         "to" => $token['token'],
@@ -38,7 +38,7 @@ if ($subscribeResult) {
     curl_exec($ch);
 
     }
-// }
+}
 
 
 $sql = "INSERT INTO android_menu(name, title, image) values('$name', '$title', '$image')";
