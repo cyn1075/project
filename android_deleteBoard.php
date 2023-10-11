055e9755c51bdd5c54c169d5f610217066aef65e
cyn1075 <126940981+cyn1075@users.noreply.github.com>
2023-10-11 20:39:42 +0900

    Add files via upload

--- android_deleteBoard.php
+++ android_deleteBoard.php
@@ -5,7 +5,7 @@ $db = mysqli_connect('13.124.103.127', 'choi', 'choichoi', 'mysql');
 $name = $_POST['userName'];
 $sql = "delete from android_menu where name='$name'";
 $sql1 = "SET @count=0";
-$sql2 = "update escape2 set idx =@count:=@count+1";
+$sql2 = "update android_menu set idx =@count:=@count+1";
 $sql3 = "ALTER TABLE idx AUTO_INCREMENT = 1";
 
 $result = mysqli_query($db, $sql);
