23381523f96c0ecd28943577c4db779b48a0549b
cyn1075 <126940981+cyn1075@users.noreply.github.com>
2023-10-11 23:01:44 +0900

    Add files via upload

--- android_subChk.php
+++ android_subChk.php
@@ -5,7 +5,7 @@ $db = mysqli_connect('13.124.103.127', 'choi', 'choichoi', 'mysql');
 $name = $_POST['myName'];
 $subName = $_POST['subscribeName'];
 
-$sql = "select EXISTS(select from android_subscribe where myid='$name' and subscribeid='$subName') ";
+$sql = " select EXISTS (select * from android_subscribe where myid='$name' and subscribeid='$subName') ";
 
 $result = mysqli_query($db, $sql);
 
