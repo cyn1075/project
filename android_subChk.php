5e1506f13c3de7a6db505a0b4b250b815dd763f1
cyn1075 <126940981+cyn1075@users.noreply.github.com>
2023-10-11 22:25:35 +0900

    Add files via upload

---
+++ android_subChk.php
@@ -0,0 +1,18 @@
+<?php 
+
+$db = mysqli_connect('13.124.103.127', 'choi', 'choichoi', 'mysql');
+
+$name = $_POST['myName'];
+$subName = $_POST['subscribeName'];
+
+$sql = "select * from android_subscribe where myid='$name' and subscribeid='$subName' ";
+
+$result = mysqli_query($db, $sql);
+
+if($result){
+    echo "1";
+}else{
+    echo"실패";
+}
+
+?>
