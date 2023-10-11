251ebf054a006cea5777d7abce94976c2a60b3d1
cyn1075 <126940981+cyn1075@users.noreply.github.com>
2023-10-11 18:22:35 +0900

    Add files via upload

--- sendCommentFCMRequest.php
+++ sendCommentFCMRequest.php
@@ -4,10 +4,12 @@
 
 //  $token= $_GET["Token"]; //해당 Device TokenKey
 
+//cievHIPYT9G_BDoGPbq_VD:APA91bHOCIpdsGNgxoL63uUl9onQsueW3DILt3IGg7RgAIGf78Qt_fl3iEykCUwz2K_0MB75psUjq76zKhJH3fLFJcDxi5jskJCgffgnxhaVyy3rbslmsagMhq0FOZ3UprVEQ2GseQin
+
 $ch = curl_init("https://fcm.googleapis.com/fcm/send");
         $header = array("Content-Type:application/json", "Authorization: key=AAAAc_IEV4A:APA91bFrBDHi3pbT7Qh8YmD3vfzlK4YQ-QZaO6Ce8LVfLrfiLK-gfRz9naUurejx8MqhItaR2HFDkRE_Av-Rad4yquHCrxufh7_BVTK3y4xw6b4qiGmRNIMoYz2l-9-tlnQ1QBGehGws");
         $data = json_encode(array(
-            "to" => "cievHIPYT9G_BDoGPbq_VD:APA91bHOCIpdsGNgxoL63uUl9onQsueW3DILt3IGg7RgAIGf78Qt_fl3iEykCUwz2K_0MB75psUjq76zKhJH3fLFJcDxi5jskJCgffgnxhaVyy3rbslmsagMhq0FOZ3UprVEQ2GseQin",
+            "to" => "f5HNphH5QiW2xCiG86hc-K:APA91bG-ogV9qmUL2u5O8Oh7zZqRLxq2UrqqV7ap_jscXrnFAk7N3oFrsRp02V7whJKYlbrXbEAoGXF-bXfsvtpYUnUy7HVken3VSTtO_qOmSPpREXXiiay9wOA5ORAhcVqrEEqmkxt7",
             "notification" => array(
                 "title"   => "test",
                 "body" => "testBody")
