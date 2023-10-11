fd6b282c871f90ca789024c7e789d799cab5c451
cyn1075 <126940981+cyn1075@users.noreply.github.com>
2023-10-12 03:33:42 +0900

    Add files via upload

--- android_menu.php
+++ android_menu.php
@@ -17,7 +17,7 @@ if ($subscribeResult) {
     // $header = array("Content-Type:application/json", "Authorization: key=AAAAc_IEV4A:APA91bFrBDHi3pbT7Qh8YmD3vfzlK4YQ-QZaO6Ce8LVfLrfiLK-gfRz9naUurejx8MqhItaR2HFDkRE_Av-Rad4yquHCrxufh7_BVTK3y4xw6b4qiGmRNIMoYzYz2l-9-tlnQ1QBGehGws");
 
 
-    // $member = mysqli_fetch_array($subscribeResult);
+    $member = mysqli_fetch_array($subscribeResult);
     // // echo $member['token'];
 
     // // $token = $subscribeResult->fetch_array(MYSQLI_ASSOC);
@@ -46,7 +46,7 @@ if ($subscribeResult) {
     $ch = curl_init("https://fcm.googleapis.com/fcm/send");
     $header = array("Content-Type:application/json", "Authorization: key=AAAAc_IEV4A:APA91bFrBDHi3pbT7Qh8YmD3vfzlK4YQ-QZaO6Ce8LVfLrfiLK-gfRz9naUurejx8MqhItaR2HFDkRE_Av-Rad4yquHCrxufh7_BVTK3y4xw6b4qiGmRNIMoYz2l-9-tlnQ1QBGehGws");
     $data = json_encode(array(
-        "to" => "f5HNphH5QiW2xCiG86hc-K:APA91bG-ogV9qmUL2u5O8Oh7zZqRLxq2UrqqV7ap_jscXrnFAk7N3oFrsRp02V7whJKYlbrXbEAoGXF-bXfsvtpYUnUy7HVken3VSTtO_qOmSPpREXXiiay9wOA5ORAhcVqrEEqmkxt7",
+        "to" => $member['token'],
         "notification" => array(
             "title"   => "test",
             "body" => "testBody")
