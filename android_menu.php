d094a0ee77cd7c60fd5d5789c46ad0be7b973c3d
cyn1075 <126940981+cyn1075@users.noreply.github.com>
2023-10-12 03:18:28 +0900

    Add files via upload

--- android_menu.php
+++ android_menu.php
@@ -18,16 +18,16 @@ if ($subscribeResult) {
 
 
     $member = mysqli_fetch_array($subscribeResult);
-    echo $member['token'];
+    // echo $member['token'];
 
     // $token = $subscribeResult->fetch_array(MYSQLI_ASSOC);
 
     // while ($row = mysqli_fetch_assoc($subscribeResult)) {
-    // $token = $row['token'];
+    $token = $member['token'];
     
 
     $data = json_encode(array(
-        "to" => $member['token'],
+        "to" => $token,
         "notification" => array(
             "title" => "$name 님이 방송을 시작했습니다.",
             "body" => "$title"
@@ -41,6 +41,30 @@ if ($subscribeResult) {
     curl_exec($ch);
 
     }
+
+
+    // $ch = curl_init("https://fcm.googleapis.com/fcm/send");
+    // $header = array("Content-Type:application/json", "Authorization: key=AAAAc_IEV4A:APA91bFrBDHi3pbT7Qh8YmD3vfzlK4YQ-QZaO6Ce8LVfLrfiLK-gfRz9naUurejx8MqhItaR2HFDkRE_Av-Rad4yquHCrxufh7_BVTK3y4xw6b4qiGmRNIMoYz2l-9-tlnQ1QBGehGws");
+    // $data = json_encode(array(
+    //     "to" => "f5HNphH5QiW2xCiG86hc-K:APA91bG-ogV9qmUL2u5O8Oh7zZqRLxq2UrqqV7ap_jscXrnFAk7N3oFrsRp02V7whJKYlbrXbEAoGXF-bXfsvtpYUnUy7HVken3VSTtO_qOmSPpREXXiiay9wOA5ORAhcVqrEEqmkxt7",
+    //     "notification" => array(
+    //         "title"   => "test",
+    //         "body" => "testBody")
+    //         ));
+    // curl_setopt($ch, CURLOPT_HTTPHEADER, $header);
+    // curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
+    // curl_setopt($ch, CURLOPT_POST, 1);
+    // curl_setopt($ch, CURLOPT_POSTFIELDS, $data);
+    // curl_exec($ch);
+    
+
+
+
+
+
+
+
+
 // }
 
 
