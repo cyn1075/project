8d70e58905e56bcee9f8bf3ceacb07794f2d906a
cyn1075 <126940981+cyn1075@users.noreply.github.com>
2023-10-11 22:58:28 +0900

    Add files via upload

--- android_subChk.php
+++ android_subChk.php
@@ -5,14 +5,23 @@ $db = mysqli_connect('13.124.103.127', 'choi', 'choichoi', 'mysql');
 $name = $_POST['myName'];
 $subName = $_POST['subscribeName'];
 
-$sql = "select * from android_subscribe where myid='$name' and subscribeid='$subName' ";
+$sql = "select EXISTS(select from android_subscribe where myid='$name' and subscribeid='$subName') ";
 
 $result = mysqli_query($db, $sql);
 
-if($result){
-    echo "1";
-}else{
-    echo"실패";
+if ($result) {
+    $row = mysqli_fetch_assoc($result);
+    $exists = $row['result'];
+    
+    if ($exists) {
+        echo "1"; // 값이 존재하면 1 출력
+    } else {
+        echo "0"; // 값이 존재하지 않으면 0 출력
+    }
+} else {
+    echo "Error: " . mysqli_error($db); // 쿼리 실행 중 오류 발생 시 오류 메시지 출력
 }
+
+mysqli_close($db);
 
 ?>
