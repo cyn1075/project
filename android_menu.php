a9a3ce54119b3e6ea84cf5107a77dbc4f0921bda
cyn1075 <126940981+cyn1075@users.noreply.github.com>
2023-10-12 02:35:14 +0900

    Add files via upload

--- android_menu.php
+++ android_menu.php
@@ -16,13 +16,12 @@ if ($subscribeResult) {
     $ch = curl_init("https://fcm.googleapis.com/fcm/send");
     $header = array("Content-Type:application/json", "Authorization: key=AAAAc_IEV4A:APA91bFrBDHi3pbT7Qh8YmD3vfzlK4YQ-QZaO6Ce8LVfLrfiLK-gfRz9naUurejx8MqhItaR2HFDkRE_Av-Rad4yquHCrxufh7_BVTK3y4xw6b4qiGmRNIMoYzYz2l-9-tlnQ1QBGehGws");
 
-    $tokens = array();
-    
 
-    while ($row = mysqli_fetch_assoc($subscribeResult)) {
-    $token = $row['token'];
-    $tokens[] = $token;
-    echo json_encode($tokens);
+    $token = $subscribeResult->fetch_array(MYSQLI_ASSOC);
+
+    // while ($row = mysqli_fetch_assoc($subscribeResult)) {
+    // $token = $row['token'];
+    // echo $token;
 
     $data = json_encode(array(
         "to" => "$token",
@@ -39,7 +38,7 @@ if ($subscribeResult) {
     curl_exec($ch);
 
     }
-}
+// }
 
 
 $sql = "INSERT INTO android_menu(name, title, image) values('$name', '$title', '$image')";
