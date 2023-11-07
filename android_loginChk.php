0a2c50a71fc3fa3bb5ae215fb7c6f9b9f12ba3fe
cyn1075 <126940981+cyn1075@users.noreply.github.com>
2023-11-07 13:51:36 +0900

    Add files via upload

---
+++ android_loginChk.php
@@ -0,0 +1,24 @@
+<?php
+
+// MySQL 연결 설정
+$con = mysqli_connect('13.124.103.127', 'choi', 'choichoi', 'mysql');
+
+$name = $_POST['myName'];
+$login = $_POST['login'];
+
+// SQL 쿼리 실행
+$sql = "update android_signup set login='$login' where name='$name'";
+
+$result = mysqli_query($con, $sql);
+
+if($result){
+    echo "성공";
+}else{
+    echo "실패";
+}
+
+mysqli_close($con);
+
+?>
+
+// 이거 내일 깃허브 올려서 확인 하기.
