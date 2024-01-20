1b06e9f23a6092a2daee1606bc4e5382b0e4cd0e
cyn1075 <126940981+cyn1075@users.noreply.github.com>
2024-01-20 15:21:58 +0900

    Add files via upload

--- android_fcm.php
+++ android_fcm.php
@@ -5,7 +5,7 @@ $con = mysqli_connect('13.124.103.127', 'choi', 'choichoi', 'mysql');
 $myname = $_POST['myName'];
 $token = $_POST['token'];
 
-$sql = "INSERT INTO android_subscribe(myid, token) values('$myname', '$token')";
+$sql = "INSERT INTO android_savefcm(myid, token) values('$myname', '$token')";
 $result = mysqli_query($con, $sql);
 
 
